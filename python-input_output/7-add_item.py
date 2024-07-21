c7625fb6ab727db4bc63b993514a1846a6d063a3
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-07-21 21:10:33 +0200

    python i/o and sql

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Are you docuemnted ?"""
+"""write script that adds to python list"""
 
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+filename = "add_item.json"
 
-import sys
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file') \
-        .load_from_json_file
+for i in argv[1:]:
+    json_list.append(argv)
 
-    try:
-        loadFile = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        loadFile = []
-
-    argc = len(sys.argv)
-    for idx in range(1, argc):
-        loadFile.append(sys.argv[idx])
-    save_to_json_file(loadFile, "add_item.json")
+save_to_json_file(json_list, filename)
