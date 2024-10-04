f2411802a00a7814a48ceb0af8809d496aa408ee
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-04 10:52:00 +0200

    bypass the cors issue

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 
 $.ajax({
-  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  url: 'https://api.allorigins.win/get?url=https://fourtonfish.com/hellosalut/?lang=fr',
   type: 'GET',
   dataType: 'json',
-  success: (json) => {
-    $('div#hello').text(json);
+  success: (response) => {
+    const json = JSON.parse(response.contents);
+    $('div#hello').text(json.hello);
+  },
+  error: (err) => {
+    console.log('Error:', err);
   }
 });
